c8a410dca91498d5c1a588fd1162ab89eccd2ac6
Christian Bager Bach Houmann <christian@bagerbach.com>
2023-02-16 11:49:54 +0100

    set up boilerplate website

---
+++ application/src/pages/index.tsx
@@ -0,0 +1,18 @@
+import { type NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>P8 Application</title>
+        <meta name="description" content="" />
+        {/* <link rel="icon" href="/favicon.ico" /> */}
+      </Head>
+      <main className="">
+      </main>
+    </>
+  );
+};
+
+export default Home;

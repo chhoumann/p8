03214ad3f3c1ce51d05fceb7b25680bd00907026
Pattrigue <pfas19@student.aau.dk>
2023-03-09 09:53:30 +0100

    Update example

--- application/src/pages/localizationExample.tsx
+++ application/src/pages/localizationExample.tsx
@@ -1,11 +1,22 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
-import "../i18n";
+import i18n from "../i18n";
 
 function LocalizationExample(): JSX.Element {
     const { t } = useTranslation();
 
-    return <div>{t("example.hello")}</div>;
+    return (
+        <div>
+            <div>{t("example.hello")}</div>
+            <button
+                onClick={() =>
+                    i18n.changeLanguage(i18n.language === "da" ? "en" : "da")
+                }
+            >
+                {t("example.changeLanguage")}
+            </button>
+        </div>
+    );
 }
 
 export default LocalizationExample;

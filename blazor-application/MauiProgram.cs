134d4e6c409de817fd0ae26150c40f6ea56f65ba
Ivikhostrup <ihostr16@student.aau.dk>
2023-04-20 10:12:15 +0200

    Setup basic config for serilog

--- blazor-application/MauiProgram.cs
+++ blazor-application/MauiProgram.cs
@@ -1,5 +1,7 @@
 ﻿using BlazorBLE.Services;
 using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Events;
 
 namespace BlazorBLE;
 
@@ -8,6 +10,9 @@ public static class MauiProgram
     public static MauiApp CreateMauiApp()
     {
         var builder = MauiApp.CreateBuilder();
+
+        SetupSerilog();
+
         builder
             .UseMauiApp<App>()
             .ConfigureFonts(fonts =>
@@ -27,4 +32,20 @@ public static class MauiProgram
 
         return builder.Build();
     }
+
+    private static void SetupSerilog()
+    {
+        // How frequently logs are flushed - set to 1 second
+        var flushInterval = new TimeSpan(0, 0, 1);
+
+        var file = Path.Combine(FileSystem.AppDataDirectory, "BlazorBLELogs.log");
+
+        Log.Logger = new LoggerConfiguration()
+        .MinimumLevel.Verbose()
+        .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+        .Enrich.FromLogContext()
+        .WriteTo.File(file, flushToDiskInterval: flushInterval, encoding: System.Text.Encoding.UTF8, rollingInterval: RollingInterval.Day, retainedFileCountLimit: 22)
+        .CreateLogger();
+    }
+
 }

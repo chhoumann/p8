fcf29668ddd21d770b1bc5dedd6b45ee95d90fca
Ivikhostrup <ihostr16@student.aau.dk>
2023-04-25 13:33:39 +0200

    Update KBeaconDataTest.cs

--- BlazorBLE.Tests/KBeaconDataTest.cs
+++ BlazorBLE.Tests/KBeaconDataTest.cs
@@ -4,20 +4,20 @@ namespace BlazorBLE.Tests
 {
     public class KBeaconDataTest
     {
-        private readonly byte[] validData = new byte[] { 0x06, 0x17, 0x9c, 0xaa, 0xb5, 0xf3, 0x7e, 0x78, 0x26, 0xd3, 0xcd, 0x82, 0xe3, 0x03, 0xde, 0xcd, 0x5d, 0x3a, 0xde, 0xf4, 0xa9, 0x96, 0xd9, 0x23, 0x16 };
+        private readonly byte[] testData = new byte[] { 0x06, 0x17, 0x9C, 0xAA, 0xB5, 0xF3, 0x7E, 0x78, 0x26, 0xD3, 0xCD, 0x82, 0xE3, 0x03, 0xDE, 0xCD, 0x5D, 0x3A, 0xDE, 0xF4, 0xA9, 0x96, 0xD9, 0x23, 0x16 };
         private readonly Guid expectedUuid = new Guid("787ef3b5-d326-82cd-e303-decd5d3adef4");
         private readonly byte[] expectedCompanyId = new byte[] { 0x06, 0x17 };
-        private readonly ushort expectedMajor = 0x96a9;
-        private readonly ushort expectedMinor = 0x23d9;
+        private readonly ushort expectedMajor = 0x96A9;
+        private readonly ushort expectedMinor = 0x23D9;
         private readonly sbyte expectedTxPower = 0x16;
 
         [Fact]
-        public void TestConstructor_ValidData_LittleEndian()
+        public void TestConstructor_testData_LittleEndian()
         {
-            // Act
-            KBeaconData kBeaconData = new KBeaconData(validData);
+            
+            KBeaconData kBeaconData = new KBeaconData(testData);
 
-            // Assert
+            
             Assert.Equal(expectedUuid, kBeaconData.Uuid);
             Assert.Equal(expectedCompanyId, kBeaconData.CompanyId);
             Assert.Equal(expectedMajor, kBeaconData.Major);
@@ -29,18 +29,18 @@ namespace BlazorBLE.Tests
         [InlineData(null)]
         [InlineData(new byte[] { })]
         [InlineData(new byte[] { 0x01, 0x02, 0x03 })]
-        public void KBeaconData_InvalidData_ThrowsArgumentException(byte[] generatedData)
+        public void KBeaconData_IntestData_ThrowsArgumentException(byte[] generatedData)
         {
             Assert.Throws<ArgumentException>(() => new KBeaconData(generatedData));
         }
 
         [Fact]
-        public void TestConstructor_ValidData_BigEndian()
+        public void TestConstructor_testData_BigEndian()
         {
-            // Act
-            KBeaconData kBeaconData = new KBeaconData(validData, false);
+            
+            KBeaconData kBeaconData = new KBeaconData(testData, false);
 
-            // Assert
+            
             Assert.Equal(expectedUuid, kBeaconData.Uuid);
             Assert.Equal(expectedCompanyId, kBeaconData.CompanyId);
             Assert.Equal(expectedMajor, kBeaconData.Major);
@@ -49,10 +49,10 @@ namespace BlazorBLE.Tests
         }
 
         [Fact]
-        public void KBeaconData_ValidData_CompanyId_LittleEndian_Equals_BigEndian()
+        public void KBeaconData_testData_CompanyId_LittleEndian_Equals_BigEndian()
         {
-            KBeaconData littleEndianKBeaconData = new KBeaconData(validData, true);
-            KBeaconData bigEndianKBeaconData = new KBeaconData(validData, false);
+            KBeaconData littleEndianKBeaconData = new KBeaconData(testData, true);
+            KBeaconData bigEndianKBeaconData = new KBeaconData(testData, false);
 
             Assert.Equal(littleEndianKBeaconData.CompanyId, bigEndianKBeaconData.CompanyId);
         }

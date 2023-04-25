c6d7887aac35dfbb4f11984d2ff408f43a0d236a
Ivikhostrup <ihostr16@student.aau.dk>
2023-04-25 11:08:44 +0200

    Update KBeaconDataTest.cs

--- BlazorBLE.Tests/KBeaconDataTest.cs
+++ BlazorBLE.Tests/KBeaconDataTest.cs
@@ -32,7 +32,24 @@ namespace BlazorBLE.Tests
         [Fact]
         public void TestConstructor_ValidData_BigEndian()
         {
+            // Arrange
+            byte[] data = new byte[] { 0x06, 0x17, 0x9c, 0xaa, 0xb5, 0xf3, 0x7e, 0x78, 0x26, 0xd3, 0xcd, 0x82, 0xe3, 0x03, 0xde, 0xcd, 0x5d, 0x3a, 0xde, 0xf4, 0xa9, 0x96, 0xd9, 0x23, 0x16 };
 
+            Guid expectedUuid = new Guid("b5f37e78-26d3-cd82-e303-decd5d3adef4");
+            byte[] expectedCompanyId = new byte[] { 0x06, 0x17 };
+            ushort expectedMajor = 0xa996;
+            ushort expectedMinor = 0xd923;
+            sbyte expectedTxPower = 0x16;
+
+            // Act
+            var KBeaconData = new KBeaconData(data, false);
+
+            // Assert
+            Assert.Equal(expectedUuid, KBeaconData.Uuid);
+            Assert.Equal(expectedCompanyId, KBeaconData.CompanyId);
+            Assert.Equal(expectedMajor, KBeaconData.Major);
+            Assert.Equal(expectedMinor, KBeaconData.Minor);
+            Assert.Equal(expectedTxPower, KBeaconData.TxPower);
         }
     }
 }

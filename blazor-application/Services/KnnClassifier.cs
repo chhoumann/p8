642672908d4babe20fbe8e5fcd56b25122c3e118
Pattrigue <patrick@gamehouse.dk>
2023-05-04 15:46:27 +0200

    Update KnnClassifier.cs
    Co-Authored-By: Christian Bager Bach Houmann <christian@bagerbach.com>

--- blazor-application/Services/KnnClassifier.cs
+++ blazor-application/Services/KnnClassifier.cs
@@ -21,6 +21,8 @@ public sealed class KnnClassifier
 
     public ClassLabel Classify(int[] rssis, IReadOnlyList<DataPoint> rssiDataPoints)
     {
+        if (k > rssis.Length) return ClassLabel.Outside;
+        
         DataPointDistance[] distances = CalculateDistances(rssis, rssiDataPoints);
         DataPointDistance[] kNearestNeighbors = GetKNearestNeighbors(distances, k);
 
